96c349581ebf0f8bd380985c4c27a7a4ff7da9f5
Amey Satwe <amey10051999@gmail.com>
2024-09-30 11:46:30 -0400

    added programmatic security

--- src/main/java/com/example/assignment2/ShoppingCart.java
+++ src/main/java/com/example/assignment2/ShoppingCart.java
@@ -11,78 +11,126 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Base64;
 
 @WebServlet(name="shoppingCartServlet",urlPatterns = "/shoppingCart.html")
 public class ShoppingCart extends HttpServlet {
+    private boolean areEqualReversed(String s1, String s2) {
+        s2 = (new StringBuffer(s2)).reverse().toString();
+        return((!s1.isEmpty()) && s1.equals(s2));
+    }
+
+    private void askForPassword(HttpServletResponse response) {
+        response.setStatus(response.SC_UNAUTHORIZED); // I.e., 401
+        response.setHeader("WWW-Authenticate",
+                "BASIC realm=\"shoppingCart\"");
+    }
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 //        super.doGet(req, resp);
-        PrintWriter out = resp.getWriter();
-        showHomePage(out);
-        out.println("    <div class='cart-container'>");
-        out.println("      <h2>Your Cart</h2>");
-        out.println("      <p>Your cart is currently empty.</p>");
-        out.println("      <!-- Future items will be added dynamically here -->");
-        out.println("    </div>");
-        out.println("  </body>");
-        out.println("</html>");
-        out.println("");
+        String authorization = req.getHeader("Authorization");
+        if(authorization==null){
+            askForPassword(resp);
+        }else {
+            String userInfo = authorization.substring(6).trim();
+//        BASE64Decoder decoder = new BASE64Decoder();
+            String nameAndPassword =
+                    new String(Base64.getDecoder().decode(userInfo));
+            // Decoded part looks like "username:password".
+            int index = nameAndPassword.indexOf(":");
+            String user = nameAndPassword.substring(0, index);
+            String password = nameAndPassword.substring(index + 1);
+            // High security: username must be reverse of password.
+            if (areEqualReversed(user, password)) {
+                PrintWriter out = resp.getWriter();
+                showHomePage(out);
+                out.println("    <div class='cart-container'>");
+                out.println("      <h2>Your Cart</h2>");
+                out.println("      <p>Your cart is currently empty.</p>");
+                out.println("      <!-- Future items will be added dynamically here -->");
+                out.println("    </div>");
+                out.println("  </body>");
+                out.println("</html>");
+                out.println("");
+            } else {
+                askForPassword(resp);
+            }
+        }
+
     }
 
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 //        super.doPost(req, resp);
+        String authorization = req.getHeader("Authorization");
+        if(authorization==null){
+            askForPassword(resp);
+        }else {
+            String userInfo = authorization.substring(6).trim();
+//        BASE64Decoder decoder = new BASE64Decoder();
+            String nameAndPassword =
+                    new String(Base64.getDecoder().decode(userInfo));
+            // Decoded part looks like "username:password".
+            int index = nameAndPassword.indexOf(":");
+            String user = nameAndPassword.substring(0, index);
+            String password = nameAndPassword.substring(index + 1);
+            // High security: username must be reverse of password.
+            if (areEqualReversed(user, password)) {
+                PrintWriter out = resp.getWriter();
+                showHomePage(out);
+                String[] products = req.getParameterValues("product");
+                String action = req.getParameter("action");
+                if(action.equals("add")){
+                    if(products!=null){
 
-        PrintWriter out = resp.getWriter();
-        showHomePage(out);
-        String[] products = req.getParameterValues("product");
-        String action = req.getParameter("action");
-        if(action.equals("add")){
-            if(products!=null){
-
-                HttpSession sess = req.getSession();
-                sess.setAttribute("products",products);
-                System.out.println(sess.getId());
-            }
-        }
-        else if(action.equals("remove")){
-            if (products!=null){
-                System.out.println("here");
-                HttpSession remSess = req.getSession();
-                String[] prods = (String[]) remSess.getAttribute("products");
-                if(prods!=null){
-                    System.out.println(remSess.getId()+prods.length);
-                    ArrayList<String> list = new ArrayList<>(Arrays.asList(prods));
-                    list.removeAll(Arrays.asList(products));
-                    remSess.setAttribute("products",list.toArray(new String[0]));
-                    System.out.println("remove len"+((String[]) remSess.getAttribute("products")).length);
+                        HttpSession sess = req.getSession();
+                        sess.setAttribute("products",products);
+                        System.out.println(sess.getId());
+                    }
                 }
+                else if(action.equals("remove")){
+                    if (products!=null){
+                        System.out.println("here");
+                        HttpSession remSess = req.getSession();
+                        String[] prods = (String[]) remSess.getAttribute("products");
+                        if(prods!=null){
+                            System.out.println(remSess.getId()+prods.length);
+                            ArrayList<String> list = new ArrayList<>(Arrays.asList(prods));
+                            list.removeAll(Arrays.asList(products));
+                            remSess.setAttribute("products",list.toArray(new String[0]));
+                            System.out.println("remove len"+((String[]) remSess.getAttribute("products")).length);
+                        }
+                    }
+                }
+                HttpSession getSess = req.getSession();
+
+                String[] getProds = (String[])getSess.getAttribute("products");
+                System.out.println(getSess.getId());
+                System.out.println(getProds.length);
+                out.println("    <div class='cart-container'>");
+                out.println("      <h2>Your Cart</h2>");
+                if (getProds.length > 0) {
+                    out.println("      <p>Your cart currently has " + getProds.length + " items.</p>");
+                } else {
+                    out.println("      <p>Your cart currently has is currently empty.</p>");
+                }
+                out.println("<ul>");
+                for(String p:getProds){
+                    out.println("<li> " + p + "</l1>");
+                }
+                out.println("</ul>");
+                out.println("    </div>");
+                out.println("  </body>");
+                out.println("</html>");
+                out.println("");
+
+
+
+            } else {
+                askForPassword(resp);
             }
         }
-        HttpSession getSess = req.getSession();
-
-        String[] getProds = (String[])getSess.getAttribute("products");
-        System.out.println(getSess.getId());
-        System.out.println(getProds.length);
-        out.println("    <div class='cart-container'>");
-        out.println("      <h2>Your Cart</h2>");
-        if (getProds.length > 0) {
-            out.println("      <p>Your cart currently has " + getProds.length + " items.</p>");
-        } else {
-            out.println("      <p>Your cart currently has is currently empty.</p>");
-        }
-        out.println("<ul>");
-        for(String p:getProds){
-            out.println("<li> " + p + "</l1>");
-        }
-        out.println("</ul>");
-        out.println("    </div>");
-        out.println("  </body>");
-        out.println("</html>");
-        out.println("");
-
-
     }
 
     public void showHomePage(PrintWriter out){
